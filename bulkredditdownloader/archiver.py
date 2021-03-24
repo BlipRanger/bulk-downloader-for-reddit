abca1af396979cde64c125cf0a7f4ac29a690d04
BlipRanger <gcs.player@gmail.com>
2021-03-23 21:34:34 -0400

    fix that too

--- bulkredditdownloader/archiver.py
+++ bulkredditdownloader/archiver.py
@@ -71,7 +71,6 @@ class Archiver(RedditDownloader):
         file_path = self.file_name_formatter.format_path(resource, self.download_directory)
         file_path.parent.mkdir(exist_ok=True, parents=True)
         details = entry.compile()
-        submission = entry.compile()['submission']
         with open(file_path, 'w') as file:
             logger.debug(f'Writing submission {entry.submission.id} to file in HTML format at {file_path}')
             html = """<html>
@@ -87,7 +86,7 @@ class Archiver(RedditDownloader):
                         <h2>Comments</h2>
                         </div>
                         <div><img href=></div>
-                        """.format(submission=submission.get('submission', ''), content=details.get('selftext', ''), url=details.get('permalink', ''), link=entry.details.get('url',''), title=details.get('title',''))
+                        """.format(submission=details.get('submission', ''), content=details.get('selftext', ''), url=details.get('permalink', ''), link=entry.details.get('url',''), title=details.get('title',''))
             for c in entry.compile()['comments']:
                 html = html + """<div id={id} class="comment">
                 <div class="info">
